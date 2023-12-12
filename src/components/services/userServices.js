d36de2f54322b713f8278babb35426775287c10a
yeison <yeisonferneyhoyos@gmail.com>
2023-12-12 12:42:09 -0500

    👌

--- src/components/services/userServices.js
+++ src/components/services/userServices.js
@@ -1,27 +1,28 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://tame-yoke-moth.cyclic.app/api'
+const BASE_URL = 'https://tame-yoke-moth.cyclic.app'
 
 // Referencia: https://stackoverflow.com/questions/71989146/axios-instance-not-getting-the-token-from-local-storage-on-first-call-react-js
 // Si existe un token, inyectalo en la cabecera de la petición
+
 axios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
+    console.log("Token from localStorage:", token); // Agrega esta línea
 
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
-
-const registerUserService = (data) => axios.post(`${BASE_URL}/users/register`, data)
-const loginUserService = (data) => axios.post(`${BASE_URL}/users/login`, data)
-const getSingleUserService = (id) => axios.get(`${BASE_URL}/users/${id}`)
-const getMeUserService = () => axios.get(`${BASE_URL}/me`)
+);
+const registerUserService = (data) => axios.post(`${BASE_URL}/api/users/register`, data)
+const loginUserService = (data) => axios.post(`${BASE_URL}/api/users/login`, data)
+const getSingleUserService = (id) => axios.get(`${BASE_URL}/api/users/${id}`)
+const getMeUserService = () => axios.get(`${BASE_URL}/api/me`)
 
 export {
   registerUserService,
