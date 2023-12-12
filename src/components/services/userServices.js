3a35fcfe4ef8741a03fce87154f2eeb8646e0075
yeison <yeisonferneyhoyos@gmail.com>
2023-12-12 12:46:39 -0500

    👀

--- src/components/services/userServices.js
+++ src/components/services/userServices.js
@@ -4,21 +4,20 @@ const BASE_URL = 'https://tame-yoke-moth.cyclic.app'
 
 // Referencia: https://stackoverflow.com/questions/71989146/axios-instance-not-getting-the-token-from-local-storage-on-first-call-react-js
 // Si existe un token, inyectalo en la cabecera de la petición
-
 axios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
-    console.log("Token from localStorage:", token); // Agrega esta línea
+    const token = localStorage.getItem('token')
 
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
-    return config;
+    return config
   },
   (error) => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
+
 const registerUserService = (data) => axios.post(`${BASE_URL}/api/users/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/api/users/login`, data)
 const getSingleUserService = (id) => axios.get(`${BASE_URL}/api/users/${id}`)
