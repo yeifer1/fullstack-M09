5e5cb71a8067e46ac64eb55723e67ae710596d15
yeison <yeisonferneyhoyos@gmail.com>
2023-12-12 12:56:35 -0500

    ..

--- src/components/Context/AuthContext.jsx
+++ src/components/Context/AuthContext.jsx
@@ -1,49 +1,51 @@
 /* eslint-disable react/prop-types */
 import { createContext, useState, useEffect } from 'react'
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
- 
+
+
 const AuthContext = createContext()
- 
+
+
 const AuthProvider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(false)
-  const [userPayload, setUsePayload] = useState(null)
- 
+  const [isAuth, setIsAuth] = useState(false) 
+  const [userPayload, setUserPayload] = useState(null) 
+
   const login = (token) => {
-    localStorage.setItem('token', token)
+    localStorage.setItem('token', token) 
     const decodedPayload = jwtDecode(token)
-    setUsePayload(decodedPayload)
+    setUserPayload(decodedPayload)
     setIsAuth(true)
   }
- 
+
   const logout = () => {
-    localStorage.removeItem('token')
-    setUsePayload(null)
-    setIsAuth(false)
+    localStorage.removeItem('token') 
+    setUserPayload(null) 
+    setIsAuth(false) 
   }
+
  
   useEffect(() => {
     const token = localStorage.getItem('token')
     if (token) {
       const decodedPayload = jwtDecode(token)
-      setUsePayload(decodedPayload)
+      setUserPayload(decodedPayload)
       setIsAuth(true)
     }
   }, [])
- 
+
   const values = {
     isAuth,
     userPayload,
     login,
     logout
   }
- 
+
   return (
     <AuthContext.Provider value={values}>
       {children}
     </AuthContext.Provider>
   )
 }
- 
+
 export { AuthProvider, AuthContext }
- 
