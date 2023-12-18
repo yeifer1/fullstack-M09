f0cede6c3100c566f7790453c3700890eb3a184c
yeison <yeisonferneyhoyos@gmail.com>
2023-12-18 11:06:45 -0500

    ...

--- src/components/services/userServices.js
+++ src/components/services/userServices.js
@@ -19,7 +19,7 @@ axios.interceptors.request.use(
 )
 
 const registerUserService = (data) => axios.post(`${BASE_URL}/api/users`, data)
-const loginUserService = (data) => axios.post(`${BASE_URL}/api/users`, data)
+const loginUserService = (data) => axios.post(`${BASE_URL}/api/users/login`, data)
 const getSingleUserService = (id) => axios.get(`${BASE_URL}/api/users${id}`)
 const getMeUserService = () => axios.get(`${BASE_URL}/api/me`)
 
