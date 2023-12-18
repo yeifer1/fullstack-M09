a588edae642e17f7218bfcce6c56a434990ee2c6
yeison <yeisonferneyhoyos@gmail.com>
2023-12-18 12:41:11 -0500

    ....

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
-import Navbar from '../../components/Navbar/Navbar'
-import Banner from '../../components/Banner/Banner'
-import Marquee from '../../components/MarqueeText/Marquee'
-import Footer from '../../components/Footer/Footer'
+import Banner from '../components/Banner/Banner'
+import Marquee from '../components/MarqueeText/Marquee'
+import Footer from '../components/Footer/Footer'
 import MoviesDetails from '../pages/MoviesDetails'
 
 const Home = () => {
   return (
     <div className='container-home'>
-      <Navbar />
+
       <Banner />
       <Marquee />
       <MoviesDetails />
